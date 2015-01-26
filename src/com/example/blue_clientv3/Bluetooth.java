0f028dd322351e7fd33658b4e334ff1ec86913de
kristof94 <christophe-dossantos@outlook.com>
2015-01-26 23:10:35 +0100

    j

---
+++ src/com/example/blue_clientv3/Bluetooth.java
@@ -0,0 +1,67 @@
+package com.example.blue_client.v2;
+
+import java.io.IOException;
+import java.util.Set;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+
+public class Bluetooth extends Thread {
+
+	BluetoothAdapter adapter;
+	BluetoothDevice device;
+	BluetoothSocket socket;
+
+	public Bluetooth() {
+
+		adapter = BluetoothAdapter.getDefaultAdapter();
+	}
+
+	public void get_paired(){
+
+		Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
+		if (pairedDevices.size() > 0) {
+			for (BluetoothDevice DEVICE : pairedDevices) {
+				if(DEVICE.getName().contains("RISTOF"))
+				{
+					device = DEVICE;
+					break;
+				}
+			}
+		}		
+	}
+
+	public void create_socket(){
+		BluetoothSocket tmp=null; 
+		try {
+			tmp = device.createRfcommSocketToServiceRecord(device.getUuids()[4].getUuid());
+		} catch (IOException e) { }
+		socket = tmp;
+	}
+
+	@Override
+	public void run() {
+		try {
+			// Connect the device through the socket. This will block
+			// until it succeeds or throws an exception
+			socket.connect();
+		} catch (IOException connectException) {
+			// Unable to connect; close the socket and get out
+			try {
+				socket.close();
+			} catch (IOException closeException) { }
+			return;
+		}
+		Log.d("blue", "Connexion établie");
+		// Do work to manage the connection (in a separate thread)
+		super.run();
+	}
+
+	public void cancel() {
+		try {
+			socket.close();
+		} catch (IOException e) { }
+	}
+}
---
+++ src/com/example/blue_clientv3/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.blue_clientv3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	String tag = "blue";
+	Bluetooth b;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	public void seConnecter(View v){
+		Log.d(tag, "se connecter");
+		b.run();
+	}
+	
+	public void Send(View v){
+		Log.d(tag, "send");
+	}
+	
+	@Override
+	protected void onStart() {
+		b = new Bluetooth();
+		super.onStart();
+	}
+
+	@Override
+	protected void onResume() {
+		b.get_paired();
+		b.create_socket();
+		super.onResume();
+	}
+
+	@Override
+	protected void onStop() {
+		b.cancel();
+		super.onStop();
+	}
+
+}
